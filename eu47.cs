08ebbdaca03543b1abc7ed138415e4ea7dbd930d
JLantis <Lantis.Wolf@gmail.com>
2018-02-13 11:35:51 +0100

    update files
    test to work with issues

--- eu47.cs
+++ eu47.cs
@@ -9,8 +9,20 @@ namespace Eu47
 	public class eu47
 	{
 		public static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
+		{	
+			string s1 = "";//enthält 1. Dateinamen
+			string s2 = "";//enhält lezten Dateinamen
+			Console.WriteLine("Erstellt eine csv oder xls Datei für eine Anzahl von Eu-Durchstrahlungen");
+			Console.WriteLine("Version 0.1");
+			Console.WriteLine("========================================================================");
+			Console.WriteLine("");
+			Console.WriteLine("ersten Dateinamen eingeben");
+			s1 = Console.ReadLine();
+			Console.WriteLine("letzten Dateinamen eingeben");
+			s2 = Console.ReadLine();
+			Console.WriteLine("von {0} bis {1}", s1, s2);
+			
+			Console.ReadLine();//um die Kommandozeile offen zu lassen
         }
 	}
 }
--- outFile.cs
+++ outFile.cs
@@ -4,15 +4,10 @@ using System.Diagnostics;
 using System.IO;
 using System.Text;
 
-namespace OutFile
+namespace Eu47
 {
 	public class outFile
 	{
-		public static void Main(string[] args)
-		{
-
-        }
-
 		private static void PrintFile(string fullPath)
 		{
 
