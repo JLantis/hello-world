df621da731aab918f0420bf686ae279539657bc5
JLantis <Lantis.Wolf@gmail.com>
2018-02-13 09:55:57 +0100

    file upload
    project eu47

---
+++ eu47.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Eu47
+{
+	public class eu47
+	{
+		public static void Main(string[] args)
+		{
+
+        }
+
+	}
+}
---
+++ outFile.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace OutFile
+{
+	public class outFile
+	{
+		public static void Main(string[] args)
+		{
+
+        }
+
+		private static void PrintFile(string fullPath)
+		{
+
+		}
+	}
+}
